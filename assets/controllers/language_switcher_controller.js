39dcbdd934687f5160e9931a85be2eba7919eeda
benjamin <benjamin.roche4@gmail.com>
2025-12-16 10:25:46 +0100

    WIP

--- assets/controllers/language_switcher_controller.js
+++ assets/controllers/language_switcher_controller.js
@@ -12,10 +12,19 @@ export default class extends Controller {
             clearTimeout(this.closeTimeout);
             this.closeTimeout = null;
         }
+
         this.menuTarget.classList.remove('hidden');
+
+        requestAnimationFrame(() => {
+            this.menuTarget.classList.remove('opacity-0', 'scale-95');
+            this.menuTarget.classList.add('opacity-100', 'scale-100');
+        });
     }
 
     close() {
+        this.menuTarget.classList.remove('opacity-100', 'scale-100');
+        this.menuTarget.classList.add('opacity-0', 'scale-95');
+
         this.closeTimeout = setTimeout(() => {
             this.menuTarget.classList.add('hidden');
         }, 150);
